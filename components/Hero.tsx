bfc444cdbea5232b3e768d8cf45077f4db02b93a
Purab Modi <purabmodi2002@gmail.com>
2023-02-21 22:35:53 +0530

    Implemented Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,10 +6,10 @@ import BackgroundCircles from './BackgroundCircles';
 export default function Hero() {
     const [text, count] = useTypewriter({
         words: [
-            "Hi, The name is Purab Modi",
-            "I am an Android Developer",
-            "Me Loves Coffee :)",
-            "<Software Developer />",
+            "Purab Modi",
+            "an Android Developer",
+            "a FreeLancer :)",
+            // "Software Engineer",
         ],
         loop: true,
         delaySpeed: 2000,
@@ -18,14 +18,16 @@ export default function Hero() {
     return (
         <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
             <BackgroundCircles />
-            <img className='relative rounded-full h-32 w-32 mx-auto object-cover' src="https://firebasestorage.googleapis.com/v0/b/bitsplease-c45fa.appspot.com/o/images%2Fpurabmodi.jpg?alt=media&token=49e97aac-6134-4bcb-9bff-7f0ca4046d84" alt="" />
+            <img
+                className='relative rounded-full h-32 w-32 mx-auto object-cover' 
+                src="https://firebasestorage.googleapis.com/v0/b/bitsplease-c45fa.appspot.com/o/images%2Fpurabmodi.jpg?alt=media&token=49e97aac-6134-4bcb-9bff-7f0ca4046d84" alt="profile_photot" />
             <div className='z-20'>
                 <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[10px]'>
                     Software Engineer
                 </h2>
                 <h1 className='text-4xl lg:text-5xl font-semibold px-10'>
                     <span className='mr-1'>
-                        {text}
+                        {`Hi, I am ${text}`}
                     </span>
                     <Cursor cursorColor='#F7AB0A' />
                 </h1>
