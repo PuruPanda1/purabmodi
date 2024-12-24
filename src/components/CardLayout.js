ba9a5cd9e9208670013ee039f4bcefb15c6b4ebf
PuruPanda1 <purabmodi2002@gmail.com>
2024-12-24 10:25:21 +0530

    Fixed modal card Layout portait mode on mobile

--- src/components/CardLayout.js
+++ src/components/CardLayout.js
@@ -79,13 +79,57 @@ function CardLayout(props) {
                                 </button>
                                 <div className="flex flex-col md:flex-row gap-8">
                                     {/* Conditionally render carousel and text based on orientation */}
-                                    {selectedProject.orientation === 'potrait' ? (
-                                        <div className="flex flex-row gap-8">
-                                            <div className="w-1/2">
-                                                <ImageCarousel images={selectedProject.images} contain={true} />
-                                            </div>
-                                            <div className="w-1/2">
+                                    {selectedProject.orientation === 'potrait'&& window.innerWidth >= 1024
+                                        ? (
+                                            <div className="flex flex-row gap-8">
+                                                <div className="w-1/2">
+                                                    <ImageCarousel images={selectedProject.images} contain={true} />
+                                                </div>
+                                                <div className="w-1/2">
 
+                                                    <h3 className="text-4xl font-bold mt-6 text-center text-gray-900 dark:text-white relative inline-block">
+                                                        {selectedProject.title}
+                                                        <span className="absolute bottom-[-6px] left-0 w-full h-1 bg-gradient-to-r from-blue-500 to-teal-500 rounded-full"></span>
+                                                    </h3>
+
+                                                    {/* Project Description */}
+                                                    <p className="text-lg text-gray-700 dark:text-gray-400 mt-4 leading-relaxed text-center max-w-3xl mx-auto">{selectedProject.long_desc}</p>
+
+                                                    {/* Project Objectives */}
+                                                    <div className="mt-6">
+                                                        <h2 className="text-2xl font-semibold text-gray-900 dark:text-white">Project Objectives</h2>
+                                                        <ul className="list-disc list-inside mt-4 space-y-2">
+                                                            {selectedProject.objectives.map((objective, index) => (
+                                                                <li key={index} className="text-gray-700 dark:text-gray-400 text-lg">{objective}</li>
+                                                            ))}
+                                                        </ul>
+                                                    </div>
+                                                    {/* Tech Stack */}
+                                                    <div className="mt-4">
+                                                        {selectedProject.techStack && (
+                                                            <div className="mt-2">
+                                                                <strong className="text-sm text-gray-700 dark:text-gray-300">Tech Stack:</strong>
+                                                                <div className="flex flex-wrap justify-center gap-2 mt-1">
+                                                                    {selectedProject.techStack.map((tech, index) => (
+                                                                        <span
+                                                                            key={index}
+                                                                            className="bg-gray-200 text-gray-800 dark:bg-gray-700 dark:text-gray-300 text-sm font-medium py-1 px-3 rounded-full"
+                                                                        >
+                                                                            {tech}
+                                                                        </span>
+                                                                    ))}
+                                                                </div>
+                                                            </div>
+                                                        )}
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            <div>
+                                                <ImageCarousel images={selectedProject.images} contain={true} />
+
+                                                {/* Project Title */}
                                                 <h3 className="text-4xl font-bold mt-6 text-center text-gray-900 dark:text-white relative inline-block">
                                                     {selectedProject.title}
                                                     <span className="absolute bottom-[-6px] left-0 w-full h-1 bg-gradient-to-r from-blue-500 to-teal-500 rounded-full"></span>
@@ -103,8 +147,9 @@ function CardLayout(props) {
                                                         ))}
                                                     </ul>
                                                 </div>
+
                                                 {/* Tech Stack */}
-                                                <div className="mt-4">
+                                                <div className="mt-6">
                                                     {selectedProject.techStack && (
                                                         <div className="mt-2">
                                                             <strong className="text-sm text-gray-700 dark:text-gray-300">Tech Stack:</strong>
@@ -120,54 +165,10 @@ function CardLayout(props) {
                                                             </div>
                                                         </div>
                                                     )}
-
                                                 </div>
+
                                             </div>
-                                        </div>
-                                    ) : (
-                                        <div>
-                                            <ImageCarousel images={selectedProject.images} contain={true} />
-
-                                            {/* Project Title */}
-                                            <h3 className="text-4xl font-bold mt-6 text-center text-gray-900 dark:text-white relative inline-block">
-                                                {selectedProject.title}
-                                                <span className="absolute bottom-[-6px] left-0 w-full h-1 bg-gradient-to-r from-blue-500 to-teal-500 rounded-full"></span>
-                                            </h3>
-
-                                            {/* Project Description */}
-                                            <p className="text-lg text-gray-700 dark:text-gray-400 mt-4 leading-relaxed text-center max-w-3xl mx-auto">{selectedProject.long_desc}</p>
-
-                                            {/* Project Objectives */}
-                                            <div className="mt-6">
-                                                <h2 className="text-2xl font-semibold text-gray-900 dark:text-white">Project Objectives</h2>
-                                                <ul className="list-disc list-inside mt-4 space-y-2">
-                                                    {selectedProject.objectives.map((objective, index) => (
-                                                        <li key={index} className="text-gray-700 dark:text-gray-400 text-lg">{objective}</li>
-                                                    ))}
-                                                </ul>
-                                            </div>
-
-                                            {/* Tech Stack */}
-                                            <div className="mt-6">
-                                                {selectedProject.techStack && (
-                                                    <div className="mt-2">
-                                                        <strong className="text-sm text-gray-700 dark:text-gray-300">Tech Stack:</strong>
-                                                        <div className="flex flex-wrap justify-center gap-2 mt-1">
-                                                            {selectedProject.techStack.map((tech, index) => (
-                                                                <span
-                                                                    key={index}
-                                                                    className="bg-gray-200 text-gray-800 dark:bg-gray-700 dark:text-gray-300 text-sm font-medium py-1 px-3 rounded-full"
-                                                                >
-                                                                    {tech}
-                                                                </span>
-                                                            ))}
-                                                        </div>
-                                                    </div>
-                                                )}
-                                            </div>
-
-                                        </div>
-                                    )}
+                                        )}
                                 </div>
                             </div>
                         </div>
